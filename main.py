e96a1db36ae0f4bdabd0c29a79996fe21d6c48bc
gyaneshhsh <54441281+gyaneshhsh@users.noreply.github.com>
2019-09-14 16:09:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext.commands import bot
-from discord import game
+from discord import dmass
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '-')
